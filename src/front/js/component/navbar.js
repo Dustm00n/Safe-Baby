ed6a0f0b99ac855e458e90a6194c0dcc857f2591
FrankLobo <frankeloboq@gmail.com>
2022-03-04 03:54:38 +0000

    actualizando logo, mejorando botones

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,31 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import "../../styles/navbar.css";
+import LogoBebe from "../../img/logo-bebe.png"
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar">
-				<Link to="/Home">
-					<span className="navbar-brand mb-0 h1 ms-4 ">logo</span>
-				</Link>
-			<div className="container mr-5 d-grid gap-2 d-md-flex justify-content-md-end">
+		<nav className="navbar navbar py-3">
+			<Link to="/Home">
+				<img className="navbar-brand" src={LogoBebe} />
+			</Link>
+			<div className="container-buttons gap-4 d-flex justify-content-end">
 				<div className="">
 					<Link to="/Home">
-						<button className="btn btn-primary fs-6 text">inicio</button>
+						<button className="btn btn-navbar text">Inicio</button>
 					</Link>
 				</div>
 				<div className="">
 					<Link to="/Process">
-						<button className="btn btn-primary">progeso</button>
+						<button className="btn btn-navbar">Progeso</button>
 					</Link>
 				</div>
 				<div className="">
 					<Link to="/Foro">
-						<button className="btn btn-primary">foro</button>
+						<button className="btn btn-navbar">Foro</button>
 					</Link>
 				</div>
 				<div className="">
 					<Link to="/Citas">
-						<button className="btn btn-primary">citas</button>
+						<button className="btn btn-navbar">Citas</button>
 					</Link>
 				</div>
 			</div>
--- src/front/js/pages/signup.js
+++ src/front/js/pages/signup.js
@@ -1,30 +1,32 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
-import "../../styles/signup.css"
 import { Context } from "../store/appContext";
+import "../../styles/signup.css";
 
 export const SignUp = () => {
     const { store, actions } = useContext(Context);
 
     return (
         <>
-            <div className="form-container">
-                <h1 className="title-login">Sign Up</h1>
-                <form>
-                    <div class="mb-3">
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nombre" />
-                    </div>
-                    <div class="mb-3">
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Apellido" />
-                    </div>
-                    <div class="mb-3">
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email" />
-                    </div>
-                    <div class="mb-3">
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
-                    </div>
-                    <button type="submit" class="btn btn-signup d-grid gap-2 col-6 mx-auto">Sign up</button>
-                </form>
+            <div className="main-login">
+                <div className="form-container">
+                    <h1 className="title-login">Sign Up</h1>
+                    <form>
+                        <div class="mb-3">
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nombre" />
+                        </div>
+                        <div class="mb-3">
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Apellido" />
+                        </div>
+                        <div class="mb-3">
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email" />
+                        </div>
+                        <div class="mb-3">
+                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+                        </div>
+                        <button type="submit" class="btn btn-signup d-grid gap-2 col-6 mx-auto">Sign up</button>
+                    </form>
+                </div>
             </div>
         </>
     );
