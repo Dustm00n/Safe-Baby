4e67314b6eaa0c805aa5789f99c6c9625c8121d1
FrankLobo <frankeloboq@gmail.com>
2022-03-15 08:12:08 +0000

    agregando estilos

--- src/front/js/component/etapas-grafico-actividades.js
+++ src/front/js/component/etapas-grafico-actividades.js
@@ -41,21 +41,3 @@ export const EtapasGraficoActividades = () => {
     );
 
 };
-
-// const data = {
-//     labels: [
-//         'Red',
-//         // 'Blue',
-//         // 'Yellow'
-//     ],
-//     datasets: [{
-//         label: 'My First Dataset',
-//         data: [300, 50, 100],
-//         backgroundColor: [
-//             'rgb(207, 73, 73)',
-//             // 'rgb(70, 173, 191)',
-//             // 'rgb(240, 190, 190)'
-//         ],
-//         hoverOffset: 5
-//     }],
-// };
