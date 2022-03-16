6071ae5f1d2cc0f0474fa67512f02ff77026b988
Dustm00n <gerardoserranosoto@gmail.com>
2022-03-16 03:11:07 +0000

    avance perfil rapido

---
+++ src/front/js/component/perfil2-informacioon.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../../styles/perfil2-info.css";
+
+const InfoBB = () => {
+    return (
+
+        <div className="card-InfoBB" >
+            <div className="card-body-InfoBB">
+                <h5 className="card-title-InfoBB">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+    )
+}
+
+export default InfoBB
--- src/front/js/pages/perfil.js
+++ src/front/js/pages/perfil.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Profile from "../component/perfil";
+import InfoBB from "../component/perfil2-informacioon"
 
 export const Perfil = () => {
     return (
         <>
             <Profile />
+            <InfoBB />
         </>
     )
 }
