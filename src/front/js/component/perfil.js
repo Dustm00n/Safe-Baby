6809e1a58adc4470dc4a12cf7ce03e551f0a2be7
Dustm00n <gerardoserranosoto@gmail.com>
2022-03-16 02:38:53 +0000

    perfil bonito entre comillas

---
+++ src/front/js/component/perfil.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../../styles/Portada.css";
+import Perfil from "../../img/Perfil.png";
+
+
+const Profile = () => {
+    return (
+        <div className="container-profile">
+            <div className="col-12">
+                <div className="card-profile">
+                    <div className="card-body-profile">
+                        <img src={Perfil} className="Foto-Perfil" />
+                        <h6 className="card-nombre mb-2 text-muted">dustm00n</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile
