fa211afb8b59702c2b514aa197613241f9c9aaa6
Dustm00n <gerardoserranosoto@gmail.com>
2022-03-16 18:57:00 +0000

    perfil 0.2

--- src/front/js/component/perfil2-informacioon.js
+++ src/front/js/component/perfil2-informacioon.js
@@ -6,16 +6,14 @@ const InfoBB = () => {
         <div className="card-InfoBB">
             <div className="card-body-InfoBB">
                 <h5 className="card-title-InfoBB">Nombre de tu Bebé</h5>
-                <p className="card-text">Matteo serrano orellana</p>
-                <div className="Boton-Baby">
-                    <button type="button" className="btn btn-secondary btn-sm boton-InfoBB">
-                        <img
-                            src="https://i0.wp.com/i.blogs.es/c51839/gateo/450_1000.gif"
-                            className="imagen-boton"
-                        />
-                        <p className="card-text-boton">Agenda Pediatrica</p>
-                    </button>
-                </div>
+                <p className="card-text-color">Matteo serrano orellana</p>
+                <button type="button" className="btn btn-secondary btn-sm btn-neon">
+                    <img
+                        src="https://i0.wp.com/i.blogs.es/c51839/gateo/450_1000.gif"
+                        className="imagen-boton"
+                    />
+                    <p className="card-text-boton">Agenda Pediatrica</p>
+                </button>
             </div>
         </div>
     );
