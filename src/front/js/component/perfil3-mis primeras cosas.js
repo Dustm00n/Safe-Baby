1026ccbba0a9b9147258bf1ce4d31a98b34cf675
Dustm00n <gerardoserranosoto@gmail.com>
2022-03-17 03:38:51 +0000

    perfil 0.4

---
+++ src/front/js/component/perfil3-mis primeras cosas.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Liston from "../../img/Liston-baby.png";
+
+
+
+const misPrimerascosas = () => {
+    return (
+        <>
+            <div className="card text-center">
+                <div className="card-header">
+                    <img src={Liston} className="liston-a" />
+                </div>
+                <div className="card-body">
+                    <h5 className="card-title">Special title treatment</h5>
+                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+                <div className="card-footer text-muted">
+                    2 days ago
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default misPrimerascosas
