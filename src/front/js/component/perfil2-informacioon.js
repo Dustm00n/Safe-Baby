eea7cd1bc5076962c04a4e0df69e1324f959e6ba
Dustm00n <gerardoserranosoto@gmail.com>
2022-03-17 01:34:07 +0000

    perfil 0.3

--- src/front/js/component/perfil2-informacioon.js
+++ src/front/js/component/perfil2-informacioon.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../../styles/perfil2-info.css";
+import { Link } from "react-router-dom";
 
 const InfoBB = () => {
     return (
@@ -7,13 +8,15 @@ const InfoBB = () => {
             <div className="card-body-InfoBB">
                 <h5 className="card-title-InfoBB">Nombre de tu Bebé</h5>
                 <p className="card-text-color">Matteo serrano orellana</p>
-                <button type="button" className="btn btn-secondary btn-sm btn-neon">
-                    <img
-                        src="https://i0.wp.com/i.blogs.es/c51839/gateo/450_1000.gif"
-                        className="imagen-boton"
-                    />
-                    <p className="card-text-boton">Agenda Pediatrica</p>
-                </button>
+                <Link to='/Agenda' >
+                    <button type="button" className="btn btn-secondary btn-sm btn-neon">
+                        <img
+                            src="https://i0.wp.com/i.blogs.es/c51839/gateo/450_1000.gif"
+                            className="imagen-boton"
+                        />
+                        <p className="card-text-boton">Agenda Pediatrica</p>
+                    </button>
+                </Link>
             </div>
         </div>
     );
---
+++ src/front/js/pages/agenda.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../../styles/building.css";
+
+
+export const Agenda = () => {
+    return (
+        <>
+
+            <img className="fotiko" text="EN CONTRUCCION" src="https://media.giphy.com/media/Yj6d4OMmDV3bnYtOow/giphy.gif"
+
+            />
+
+        </>
+    )
+};
