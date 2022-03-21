df8d8bdcc3ac399b086bb1d90a8a0960a49d1de1
Dustm00n <gerardoserranosoto@gmail.com>
2022-03-21 06:08:40 +0000

    completo

--- src/front/js/component/perfil3-mis primeras cosas.js
+++ src/front/js/component/perfil3-mis primeras cosas.js
@@ -6,7 +6,7 @@ import FormularioUpoad from "../component/formularioparasubirarchivo"
 const Misprimerascosas = () => {
     return (
         <>
-            <div className="card-P">
+            {/* <div className="card-P">
                 <div className="header-liston">
                     <img src={Liston} className="Listoncito" />
                 </div>
@@ -17,8 +17,8 @@ const Misprimerascosas = () => {
                                 <img src="https://sp-ao.shortpixel.ai/client/to_avif,q_glossy,ret_img,w_1065,h_580/https://www.ortodonciabarcelona.com/blog-profesional/wp-content/uploads/2014/12/Captura2Bde2Bpantalla2B2014-11-242Ba2Blas2B17.08.51-1065x580.png" className="card-img-top" />
                                 <div className="card-body">
                                     {/* <h5 className="card-title">Card title</h5> */}
-                                    {/* <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p> */}
-                                </div>
+            {/* <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p> */}
+            {/* </div>
                             </div>
                         </div>
                         <div className="col  bs">
@@ -51,7 +51,11 @@ const Misprimerascosas = () => {
                         < FormularioUpoad />
                     </div>
                 </div>
+            </div> */} *
+            <div className="card   chiquita2">
+                <img className="fotiko2" text="EN CONTRUCCION" src="https://media.giphy.com/media/xT39D0hG7xu23NHCHC/giphy.gif" />
             </div>
+
         </>
     )
 }
